6685da9c604f9a94398d10467714581acd700027
kylevedder <kyle.c.vedder@gmail.com>
2016-12-06 15:56:39 -0500

    Moved to AGPL

--- main.cc
+++ main.cc
@@ -1,15 +1,15 @@
 // Copyright 2016 kyle@vedder.io
 //
 // This software is free: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License Version 3,
+// it under the terms of the GNU Affero General Public License Version 3,
 // as published by the Free Software Foundation.
 //
 // This software is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Lesser General Public License for more details.
+// GNU Affero General Public License for more details.
 //
-// You should have received a copy of the GNU Lesser General Public License
+// You should have received a copy of the GNU Affero General Public License
 // Version 3 in the file COPYING that came with this distribution.
 // If not, see <http://www.gnu.org/licenses/>.
 // ========================================================================
